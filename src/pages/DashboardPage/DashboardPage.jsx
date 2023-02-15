ee12ee654efb33ddae956a765cfda40fcea9128b
Yuriy Minashkin <y.minashkin@gmail.com>
2023-02-15 17:11:19 +0200

    dashboard update

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Header } from 'components/Header/Header';
+import { Navigation } from 'components/Navigation/Navigation';
+import { Balance } from 'components/Balance/Balance';
+import { Currency } from 'components/Currency/Currency';
+import { Container, SidebarWrapper, NavBalanceWrapper } from './DashboardPage.styled';
 
 export const DashboardPage = () => {
+
   return (
-    <div>DashboardPage</div>
-  )
-}
+    <>
+      <Header />
+        <Container>
+          <SidebarWrapper>
+            <NavBalanceWrapper>
+              <Navigation />
+              <Balance />
+            </NavBalanceWrapper>
+            <Currency />
+          </SidebarWrapper>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Outlet />
+          </Suspense>
+        </Container>
+    </>
+  );
+};
