c69450904be479e922405d12e707c05ed1b10236
Yuriy Minashkin <y.minashkin@gmail.com>
2023-02-15 12:28:59 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,31 @@
 import { useEffect, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { PrivateRoute, PublicRoute } from 'components/PrivatPublicRoutes/PrivatPublicRoutes';
-import { selectIsRefreshing, selectIsLoggedIn } from 'redux/auth/authSelectors';
 import { refreshUser } from 'redux/auth/authOperations';
+import { useAuth } from 'hooks/useAuth';
 import { RegistrationPage } from 'pages/RegistrationPage/RegistrationPage';
 import { LoginPage } from 'pages/LoginPage/LoginPage';
 import { DashboardPage } from 'pages/DashboardPage/DashboardPage';
 
 const HomePage = lazy(() => import('pages/HomePage/HomePage'));
-const DiagramPage = lazy(() => import('pages/DiagramPage/DiagramPage'));
+const SummaryPage = lazy(() => import('pages/SummaryPage/SummaryPage'));
 
 const App = () => {
   const dispatch = useDispatch();
-  const userRefreshing = useSelector(selectIsRefreshing);
-  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const { isRefreshing, isLoggedIn } = useAuth();
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  return userRefreshing ? ( <div>Loading...</div>) : (
-     <Routes>
-          <Route path="/register" element={<PublicRoute component={<RegistrationPage />} />} />
+  return isRefreshing ? ( <div>Loading...</div>) : (
+    <Routes>
           <Route path="/login"  element={<PublicRoute component={<LoginPage />} />} />
+          <Route path="/register" element={<PublicRoute component={<RegistrationPage />} />} />
           <Route path="/" element={<PrivateRoute component={<DashboardPage />} />} >
             <Route index element={<HomePage />} />
-            <Route path="diagram" element={<DiagramPage />} />
+            <Route path="diagram" element={<SummaryPage />} />
           </Route>
           <Route path="*" element={isLoggedIn ? <DashboardPage /> : <LoginPage />} />
         </Routes>
