818d52a8417dc91dbb97da70fe531ef8cccf0ab8
xanderkiev2022 <301019a@gmail.com>
2023-02-16 18:52:23 +0200

    styles

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -2,11 +2,20 @@ import axios from 'axios';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { Loader } from './Loader';
+import {
+  Container,
+  Table,
+  Tbody,
+  Thead,
+  RowHeading,
+  RowBody,
+} from './Currency.styled';
+
 
 //===fetch===
 const currencyAPI = async () => {
   try {return await axios.get('https://api.monobank.ua/bank/currency');} 
-  catch (error) {return alert(`Something went wrong. ${error}. Please, try again later`);}
+  catch (error) {return console.log(`Something went wrong. ${error}. Please, try again later`);}
 };
 
 //===main logic===
@@ -28,36 +37,38 @@ export const Currency = () => {
         const filterdCurrency = data.filter(item =>visibleCurrencyCode.includes(Number(item.currencyCodeA)) && item.currencyCodeB !== 840);
         localStorage.setItem(LS_KEY, JSON.stringify({ cur: filterdCurrency, fetchTime: Date.now() }));
         setCurrencyRate(JSON.parse(localStorage.getItem(LS_KEY)));} 
-      catch (error) { return alert(`Something went wrong. ${error}. Please, try again later`); }
+      catch (error) { return console.log(`Something went wrong. ${error}. Please, try again later`); }
       finally { setLoading(false); }};
       
     getCurrency();
   }, [currencyRate]);
 
   return (
-    <div>
-      <table>
-        <thead>
+    <Container>
+      <Table>
+        <Thead>
           <tr>
-            <th>Currency</th>
-            <th>Purchase</th>
-            <th>Sale</th>
+            <RowHeading>Currency</RowHeading>
+            <RowHeading>Purchase</RowHeading>
+            <RowHeading>Sale</RowHeading>
           </tr>
-        </thead>
-        {loading ? (
-          <Loader />
-        ) : (
-          <tbody>
-            {currencyRate.cur?.map(({ currencyCodeA, rateBuy, rateSell }) => (
-              <tr key={currencyCodeA}>
-                <td>{currencyCodeA === Number(840) ? 'USD' : 'EUR'}</td>
-                <td>{Number(rateBuy).toFixed(2)}</td>
-                <td>{Number(rateSell).toFixed(2)}</td>
-              </tr>
-            ))}
-          </tbody>
-        )}
-      </table>
-    </div>
+        </Thead>
+          {loading ? (
+            <Loader />
+          ) : (
+            <Tbody>
+              {currencyRate.cur?.map(({ currencyCodeA, rateBuy, rateSell }) => (
+                <tr key={currencyCodeA}>
+                  <RowBody>
+                    {currencyCodeA === Number(840) ? 'USD' : 'EUR'}
+                  </RowBody>
+                  <RowBody>{Number(rateBuy).toFixed(2)}</RowBody>
+                  <RowBody>{Number(rateSell).toFixed(2)}</RowBody>
+                </tr>
+              ))}
+            </Tbody>
+          )}
+      </Table>
+    </Container>
   );
 };
