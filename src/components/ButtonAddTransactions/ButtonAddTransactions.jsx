74a09473e9f93377cea2659b95994ee5221f4766
xanderkiev2022 <301019a@gmail.com>
2023-02-17 21:19:45 +0200

    TransactionsList
    AddButton,
    TransactionsList,
    HomeTab

--- src/components/ButtonAddTransactions/ButtonAddTransactions.jsx
+++ src/components/ButtonAddTransactions/ButtonAddTransactions.jsx
@@ -1,11 +1,23 @@
 import { useDispatch } from 'react-redux';
 import { openModalAddTransaction } from 'redux/global/globalSlice';
 import { Button } from './ButtonAddTransactions.styled';
+import { HiPlus } from 'react-icons/hi2';
 
 export const ButtonAddTransactions = () => {
   const dispatch = useDispatch();
 
   return (
-    <Button onClick={() => dispatch(openModalAddTransaction())}>Add</Button>
+    <Button onClick={() => dispatch(openModalAddTransaction())}>
+      <HiPlus
+        style={{
+          display: 'block',
+          width: '50px',
+          height: '50px',
+          backgroundColor: '#24cca7',
+          borderRadius: '50%',
+        }}
+        color="white"
+      />
+    </Button>
   );
 };
---
+++ src/components/HomeTab/HomeTab.styles.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 46px 0 0 69px;
+  width: 784px;
+`;
