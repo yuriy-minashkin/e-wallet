04589e2fa7c3834dde5242c301402ba2812f2f56
Maksym-Chmilov <maxchmilov@gmail.com>
2023-02-20 10:54:45 +0200

    login/reg styled

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -4,25 +4,46 @@ import { register } from '../../redux/auth/authOperations';
 import { Formik } from 'formik';
 import { nanoid } from 'nanoid';
 import * as yup from 'yup';
-import { FormLayout, Form, ButtonRegister } from './RegistrationForm.styled';
+import {
+  FormLayout,
+  Form,
+  ButtonRegister,
+  RegistrationInput,
+  RegistrationLabel,
+  InputIcon,
+  ButtonContainer,
+  ErrorText,
+  ButtonLogIn,
+  LogoContainer,
+} from './RegistrationForm.styled';
+import Icons from 'images/icons.svg';
+import { Logo } from 'components/Logo/Logo';
+import { toast, ToastContainer } from 'react-toastify';
 
 const validationSchema = yup.object().shape({
-  email: yup.string().email().required('E-mail is a required field'),
+  email: yup
+    .string()
+    .email('E-mail must be valid email')
+    .required('E-mail is a required field'),
   password: yup
     .string()
-    .min(6)
-    .max(12)
+    .min(6, 'Password must be at least 6 characters')
+    .max(12, 'Password must be at most 12 characters')
     .required('Password is a required field'),
   passwordConfirmation: yup
     .string()
-    .min(6)
-    .max(12)
+    .min(6, 'Password must be at least 6 characters')
+    .max(12, 'Password must be at most 12 characters')
     .oneOf(
       [yup.ref('password'), null],
       'Confirm password must match the password'
     )
     .required('Confirm password is a required field'),
-  name: yup.string().min(1).max(12).required('Please enter your name'),
+  name: yup
+    .string()
+    .min(1)
+    .max(12, 'First name must be at most 12 characters')
+    .required('Please enter your name'),
 });
 
 export const RegistrationForm = () => {
@@ -32,93 +53,115 @@ export const RegistrationForm = () => {
   const nameId = nanoid();
 
   return (
-    <FormLayout>
-      <Formik
-        initialValues={{
-          name: '',
-          email: '',
-          password: '',
-          passwordConfirmation: '',
-        }}
-        onSubmit={values => {
-          dispatch(
-            register({
-              username: values.name,
-              email: values.email,
-              password: values.password,
-            })
-          );
-        }}
-        validationSchema={validationSchema}
-      >
-        {({
-          errors,
-          touched,
-          values,
-          handleSubmit,
-          handleBlur,
-          handleChange,
-          isSubmitting,
-        }) => (
+    <Formik
+      initialValues={{
+        name: '',
+        email: '',
+        password: '',
+        passwordConfirmation: '',
+      }}
+      onSubmit={values => {
+        dispatch(
+          register({
+            username: values.name,
+            email: values.email,
+            password: values.password,
+          })
+        )
+      }}
+      validationSchema={validationSchema}
+    >
+      {({
+        errors,
+        touched,
+        values,
+        handleSubmit,
+        handleBlur,
+        handleChange,
+        isSubmitting,
+      }) => (
+        <FormLayout>
+          <LogoContainer>
+            <Logo />
+          </LogoContainer>
           <Form autoComplete="off" onSubmit={handleSubmit}>
-            <label htmlFor={emailId}>E-mail</label>
-            <input
+            <RegistrationLabel htmlFor={emailId} />
+            <RegistrationInput
               type="email"
               name="email"
               id={emailId}
               onChange={handleChange}
               onBlur={handleBlur}
               value={values.email}
-              placeholder="example@mail.com"
+              placeholder="E-mail:  example@mail.com"
             />
-            <span>{errors.email && touched.email && errors.email}</span>
-            <label htmlFor={passwordId}>Password</label>
-            <input
+            <InputIcon width="21" height="16">
+              <use href={`${Icons}#icon-email`} />
+            </InputIcon>
+            <ErrorText>
+              {errors.email && touched.email && errors.email}
+            </ErrorText>
+            <RegistrationLabel htmlFor={passwordId} />
+            <RegistrationInput
               type="password"
               name="password"
               id={passwordId}
               onChange={handleChange}
               onBlur={handleBlur}
               value={values.password}
-              placeholder="Password (from 6 to 12 symbols)"
+              placeholder="Password"
             />
-            <span>
+            <InputIcon width="16" height="21">
+              <use href={`${Icons}#icon-lock`} />
+            </InputIcon>
+            <ErrorText>
               {errors.password && touched.password && errors.password}
-            </span>
-            <label>Confirm password</label>
-            <input
+            </ErrorText>
+            <RegistrationLabel htmlFor={passwordId} />
+            <RegistrationInput
               type="password"
               name="passwordConfirmation"
               onChange={handleChange}
               onBlur={handleBlur}
               value={values.passwordConfirmation}
-              placeholder="Password (from 6 to 12 symbols)"
+              placeholder="Confirm password"
             />
-            <span>
+            <InputIcon width="16" height="21">
+              <use href={`${Icons}#icon-lock`} />
+            </InputIcon>
+            <ErrorText>
               {errors.passwordConfirmation &&
                 touched.passwordConfirmation &&
                 errors.passwordConfirmation}
-            </span>
-            <label>First name</label>
-            <input
+            </ErrorText>
+            <RegistrationLabel htmlFor={nameId} />
+            <RegistrationInput
               type="text"
               name="name"
               id={nameId}
               onChange={handleChange}
               onBlur={handleBlur}
               value={values.name}
-              placeholder="Adrian"
+              placeholder="First name:  Adrian"
             />
-            <span>{errors.name && touched.name && errors.name}</span>
-            <ButtonRegister type="submit" disabled={isSubmitting}>
-              {'Register'.toUpperCase()}
-            </ButtonRegister>
-            <Link to="/login">
-              <button type="button">{'Log in'.toUpperCase()}</button>
-            </Link>
+            <InputIcon width="18" height="18">
+              <use href={`${Icons}#icon-account_box`} />
+            </InputIcon>
+            <ErrorText>{errors.name && touched.name && errors.name}</ErrorText>
+            <ButtonContainer>
+              <ButtonRegister type="submit" disabled={isSubmitting}>
+                {'Register'.toUpperCase()}
+              </ButtonRegister>
+              <Link to="/login">
+                <ButtonLogIn type="ButtonLogIn">
+                  {'Log in'.toUpperCase()}
+                </ButtonLogIn>
+              </Link>
+            </ButtonContainer>
           </Form>
-        )}
-      </Formik>
-    </FormLayout>
+          <ToastContainer/>
+        </FormLayout>
+      )}
+    </Formik>
   );
 };
